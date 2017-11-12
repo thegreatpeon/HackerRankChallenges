3e00f90bd45e3a23d68c42569e7dcd896936a4b4
Adam Keaton <adamjkeaton@gmail.com>
2017-11-12 15:40:10 -0800

    Completed Challenge 19 - Interfaces Commiting the updated project files.

--- HackerRankChallenges/Day19.h
+++ HackerRankChallenges/Day19.h
@@ -2,3 +2,24 @@
 #include <iostream>
 
 void runDay19();
+
+class AdvancedArithmetic 
+{
+public:
+	AdvancedArithmetic(){}
+	virtual ~AdvancedArithmetic(){}
+
+	virtual int divisorSum(int n) = 0;
+};
+
+class Calculator19 : public AdvancedArithmetic
+{
+	int sum = 0;
+public:
+	Calculator19() {}
+	~Calculator19() {}
+
+	int divisorSum(int n);
+	
+
+};
