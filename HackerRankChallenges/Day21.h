70b92301f31bd100dab51ae0d0f0c82afafeb828
Adam Keaton <adamjkeaton@gmail.com>
2017-11-13 19:54:47 -0800

    Completed Challenge 21 - Generic Types

--- HackerRankChallenges/Day21.h
+++ HackerRankChallenges/Day21.h
@@ -1,4 +1,9 @@
 #pragma once
 #include <iostream>
+#include <vector>
+#include <string>
 
 void runDay21();
+
+template <class myType>
+void printArray(std::vector<myType> a);
--- HackerRankChallenges/main.cpp
+++ HackerRankChallenges/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() 
 {
 	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
-	runDay20();
+	runDay21();
 	
 	return 0;
 }
