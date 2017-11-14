949bec462ca3a149edb4693a78505cd1cddf6831
Adam Keaton <adamjkeaton@gmail.com>
2017-11-13 21:23:59 -0800

    Completed Challenge 22 - Binary Search Tree, Find the Height.

--- HackerRankChallenges/Day22.h
+++ HackerRankChallenges/Day22.h
@@ -2,3 +2,19 @@
 #include <iostream>
 
 void runDay22();
+
+class Node22 
+{
+public:
+	int data;
+	Node22 *left;
+	Node22 *right;
+	Node22(int d);
+};
+
+class Solution22
+{
+public:
+	Node22* insert(Node22* root, int data);
+	int getHeight(Node22* root);
+};
