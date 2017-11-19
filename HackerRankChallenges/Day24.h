7cbc46f590f8e747999775b28b310c23a6dc52ef
Adam Keaton <adamjkeaton@gmail.com>
2017-11-19 14:08:07 -0800

    Completed Challenge 24 - Remove duplicate values in linked list

--- HackerRankChallenges/Day24.h
+++ HackerRankChallenges/Day24.h
@@ -1,4 +1,21 @@
 #pragma once
 #include <iostream>
+#include <vector>
 
 void runDay24();
+
+class Node24
+{
+public:
+	int data;
+	Node24 *next;
+	Node24(int d);
+};
+
+class Solution24
+{
+public:
+	Node24* removeDuplicates(Node24 *head);
+	Node24* insert(Node24 *head, int data);
+	void display(Node24 *head);
+};
