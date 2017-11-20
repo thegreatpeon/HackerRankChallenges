e4d821c6a295e9347c291daef285be251409e741
Adam Keaton <adamjkeaton@gmail.com>
2017-11-19 16:33:15 -0800

    Completed Challenge 25 - Time Complexity, is number prime?

--- HackerRankChallenges/Day25.cpp
+++ HackerRankChallenges/Day25.cpp
@@ -1,7 +1,48 @@
-#include "Day15.h"
+#include "Day25.h"
 #pragma once
 
 void runDay25()
 {
+	int inputNum = 0, temp = 0;
+	std::vector<int> inputList;
+	bool prime;
 
+	std::cin >> inputNum;
+	for (int i = 0; i < inputNum; i++)
+	{
+		std::cin >> temp;
+		inputList.push_back(temp);
+	}
+
+	for (int i = 0; i < inputNum; i++)
+	{
+		temp = 2;
+		prime = true;
+
+		if (inputList.at(i) == 1)
+		{
+			prime = false;
+		}
+
+		while (temp <= sqrt(inputList.at(i)) && prime)
+		{
+			if (inputList.at(i) % temp == 0)
+			{
+				prime = false;
+			}
+			else
+			{
+				temp++;
+			}
+		}
+
+		if (prime)
+		{
+			std::cout << "Prime" << std::endl;
+		}
+		else
+		{
+			std::cout << "Not prime" << std::endl;
+		}
+	}
 }
--- HackerRankChallenges/main.cpp
+++ HackerRankChallenges/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() 
 {
 	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
-	runDay24();
+	runDay25();
 	
 	return 0;
 }
